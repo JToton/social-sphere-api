79beecf96d55332ec9d773c990a9607f1b01488b
JacobToton <Jake_Toton@live.com>
2024-07-09 18:49:40 -0600

    Created File directory, added re-usable helper util from previous homework assignments.
    Created the file structure for the assignment.  Added the folders and files I believe I will need to complete said assignment.  Updated / added node and dependencies.  Updated gitignore.

---
+++ routes/utils/formateDate.js
@@ -0,0 +1,12 @@
+function formatDate(date) {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+}
+
+module.exports = { formatDate };
