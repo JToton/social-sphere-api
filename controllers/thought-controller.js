7f6a38878a1c1193576001cf798698f3cffb6869
JacobToton <Jake_Toton@live.com>
2024-07-10 12:11:39 -0600

    Updated and Added Comments
    Updated and Added Comments to thought and user controllers.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,81 +1,97 @@
+// ? Thought Controller
 const { Thought, User } = require("../models");
 
 const thoughtController = {
-  // Get all thoughts
+  // ? Get all thoughts
   async getAllThoughts(req, res) {
     try {
+      // Retrieve all thoughts, populate the reactions, and exclude the __v field.
       const thoughts = await Thought.find({})
         .populate({ path: "reactions", select: "-__v" })
         .select("-__v");
       res.json(thoughts);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Get a single thought by id
+  // ? Get a single thought by id
   async getThoughtById(req, res) {
     try {
+      // Retrieve a single thought by its id, populate the reactions, and exclude the __v field.
       const thought = await Thought.findOne({ _id: req.params.id })
         .populate({ path: "reactions", select: "-__v" })
         .select("-__v");
       if (!thought) {
+        // Return a 404 Not Found error if the thought is not found.
         return res
           .status(404)
           .json({ message: "No thought found with this id!" });
       }
       res.json(thought);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Create a new thought
+  // ? Create a new thought
   async createThought(req, res) {
     try {
+      // Create a new thought based on the request body.
       const thought = await Thought.create(req.body);
+      // Find the user associated with the new thought and add the thought's _id to the user's thoughts array.
       const user = await User.findOneAndUpdate(
         { _id: req.body.userId },
         { $push: { thoughts: thought._id } },
         { new: true }
       );
       if (!user) {
+        // Return a 404 Not Found error if the user is not found.
         return res.status(404).json({ message: "No user found with this id!" });
       }
       res.json(thought);
     } catch (err) {
+      // Return a 400 Bad Request error if an error occurs.
       res.status(400).json(err);
     }
   },
 
-  // Update a thought by id
+  // ? Update a thought by id
   async updateThought(req, res) {
     try {
+      // Find the thought by id and update it with the request body, using the new: true and runValidators: true options.
       const thought = await Thought.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
         { new: true, runValidators: true }
       );
       if (!thought) {
+        // Return a 404 Not Found error if the thought is not found.
         return res
           .status(404)
           .json({ message: "No thought found with this id!" });
       }
       res.json(thought);
     } catch (err) {
+      // Return a 400 Bad Request error if an error occurs.
       res.status(400).json(err);
     }
   },
 
-  // Delete a thought by id
+  // ? Delete a thought by id
   async deleteThought(req, res) {
     try {
+      // Find the thought by id and delete it.
       const thought = await Thought.findOneAndDelete({ _id: req.params.id });
       if (!thought) {
+        // Return a 404 Not Found error if the thought is not found.
         return res
           .status(404)
           .json({ message: "No thought found with this id!" });
       }
+      // Remove the deleted thought's _id from the associated user's thoughts array.
       await User.findOneAndUpdate(
         { thoughts: req.params.id },
         { $pull: { thoughts: req.params.id } },
@@ -83,44 +99,51 @@ const thoughtController = {
       );
       res.json({ message: "Thought deleted!" });
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Create a reaction
+  // ? Create a reaction
   async createReaction(req, res) {
     try {
+      // Find the thought by its id and add the new reaction to its reactions array.
       const thought = await Thought.findOneAndUpdate(
         { _id: req.params.thoughtId },
         { $addToSet: { reactions: req.body } },
         { new: true, runValidators: true }
       );
       if (!thought) {
+        // Return a 404 Not Found error if the thought is not found.
         return res
           .status(404)
           .json({ message: "No thought found with this id!" });
       }
       res.json(thought);
     } catch (err) {
+      // Return a 400 Bad Request error if an error occurs.
       res.status(400).json(err);
     }
   },
 
-  // Delete a reaction
+  // ? Delete a reaction
   async deleteReaction(req, res) {
     try {
+      // Find the thought by its id and remove the specified reaction from its reactions array.
       const reactiondata = await Thought.findOneAndUpdate(
         { _id: req.params.thoughtId },
         { $pull: { reactions: { reactionId: req.params.reactionId } } },
         { new: true, runValidators: true }
       );
       if (!reactiondata) {
+        // Return a 404 Not Found error if the thought is not found.
         return res
           .status(404)
           .json({ message: "No thought found with this id!" });
       }
       res.json(reactiondata);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,67 +1,80 @@
+// ? User Controller
 const { User, Thought } = require("../models");
 
 const userController = {
-  // Get all users
+  // ? Get all users
   async getAllUsers(req, res) {
     try {
+      // Retrieve all users, populate the thoughts and friends, and exclude the __v field.
       const users = await User.find({})
         .populate({ path: "thoughts", select: "-__v" })
         .populate({ path: "friends", select: "-__v" })
         .select("-__v");
       res.json(users);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Get a single user by id
+  // ? Get a single user by id
   async getUserById(req, res) {
     try {
+      // Retrieve a single user by its id, populate the thoughts and friends, and exclude the __v field.
       const user = await User.findOne({ _id: req.params.id })
         .populate({ path: "thoughts", select: "-__v" })
         .populate({ path: "friends", select: "-__v" })
         .select("-__v");
       if (!user) {
+        // Return a 404 Not Found error if the user is not found.
         return res.status(404).json({ message: "No user found with this id!" });
       }
       res.json(user);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Create a new user
+  // ? Create a new user
   async createUser(req, res) {
     try {
+      // Create a new user based on the request body.
       const user = await User.create(req.body);
       res.json(user);
     } catch (err) {
+      // Return a 400 Bad Request error if an error occurs.
       res.status(400).json(err);
     }
   },
 
-  // Update a user by id
+  // ? Update a user by id
   async updateUser(req, res) {
     try {
+      // Find the user by id and update it with the request body, using the new: true and runValidators: true options.
       const user = await User.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
         { new: true, runValidators: true }
       );
       if (!user) {
+        // Return a 404 Not Found error if the user is not found.
         return res.status(404).json({ message: "No user found with this id!" });
       }
       res.json(user);
     } catch (err) {
+      // Return a 400 Bad Request error if an error occurs.
       res.status(400).json(err);
     }
   },
 
-  // Delete a user by id
+  // ? Delete a user by id
   async deleteUser(req, res) {
     try {
+      // Find the user by id and delete it.
       const user = await User.findOneAndDelete({ _id: req.params.id });
       if (!user) {
+        // Return a 404 Not Found error if the user is not found.
         return res.status(404).json({ message: "No user found with this id!" });
       }
 
@@ -69,40 +82,47 @@ const userController = {
       await Thought.deleteMany({ _id: { $in: user.thoughts } });
       res.json({ message: "User and associated thoughts deleted!" });
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Add a friend to a user's friend list
+  // ? Add a friend to a user's friend list
   async addFriend(req, res) {
     try {
+      // Find the user by id and add the specified friend to the user's friends array.
       const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
         { $addToSet: { friends: req.params.friendId } },
         { new: true }
       );
       if (!user) {
+        // Return a 404 Not Found error if the user is not found.
         return res.status(404).json({ message: "No user found with this id!" });
       }
       res.json(user);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
 
-  // Remove a friend from a user's friend list
+  // ? Remove a friend from a user's friend list
   async removeFriend(req, res) {
     try {
+      // Find the user by id and remove the specified friend from the user's friends array.
       const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
         { $pull: { friends: req.params.friendId } },
         { new: true }
       );
       if (!user) {
+        // Return a 404 Not Found error if the user is not found.
         return res.status(404).json({ message: "No user found with this id!" });
       }
       res.json(user);
     } catch (err) {
+      // Return a 500 Internal Server Error if an error occurs.
       res.status(500).json(err);
     }
   },
